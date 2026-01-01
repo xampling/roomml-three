383fc4e3907ae3d1d381a25e5984cc1a26460c6c
xampling <chebvron@gmail.com>
2026-01-01 22:05:59 +0100

    Dedupe React to avoid invalid hook calls

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ command }) => ({
   base: command === 'build' ? '/hsml/' : '/',
   plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom']
+  },
   server: {
     host: '0.0.0.0'
   }
